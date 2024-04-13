8a7231f3736047073de66e65b698e305c74d10ac
LucasMezza <mezza.lucas12@gmail.com>
2024-04-13 16:48:57 -0300

    mail js

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const btnEnviar = document.querySelector('[data-btn]'),
-      navbar = document.querySelector('.navbar'),
-      info = {
+const btnEnviar   = document.querySelector('[data-btn]'),
+      navbar      = document.querySelector('.navbar'),
+      form        = document.querySelector('[data-form]'),
+      inputNombre = document.querySelector('input[name="nombre"]'),
+      inputAsunto = document.querySelector('input[name="asunto"]'),
+      textarea    = document.querySelector('textarea[name="message"]'),
+      info        = {
           nombre : '',
           asunto : '',
           message: '',
           mail   : ''  
         };
 
-let flag = true;
-window.addEventListener('scroll', () => {
-
-    if(window.scrollY == 0 && !flag || window.scrollY > 0 && flag) {
-        navbar.classList.toggle('top');
-        flag = !flag;
+const debounce = (callback) => {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback(...args)    
+        }, 1000);
     }
+}
 
-}, true);
+const createMail = (event) => {
+    info[event.target.name] = event.target.value;
 
-
-input.addEventListener('change', event => {
-    event.preventDefault();
-
-    setTimeout(() => {
-        info[input.name] = input.value;     
-    }, 500);
-
-    const form     = document.closest('[data-form]'),
-          invalids = form.querySelectorAll('input:required:invalid,textarea:required:invalid');
+    const invalids = form.querySelectorAll('input:required:invalid,textarea:required:invalid');
 
     if(invalids.length > 0) return;
 
     let mail;
-
+    
+    info.mail = document.querySelector('input[name="mail"]:checked').value;
     if(info.mail == "gmail") {
         mail = `https://mail.google.com/mail/u/0/?fs=1&to=resolveinfo.dev@gmail.com&su=${info.asunto}&body=${info.nombre}${info.message}&tf=cm`;
     }
@@ -41,8 +40,20 @@ input.addEventListener('change', event => {
     }
 
     console.log(mail)
-    btnEnviar.setAttribute('href', encodeURI(mail))
+    btnEnviar.setAttribute('href', encodeURI(mail));
     btnEnviar.classList.remove('disabled');
-});
+}
+        
+let flag = true;
+window.addEventListener('scroll', () => {
 
+    if(window.scrollY == 0 && !flag || window.scrollY > 0 && flag) {
+        navbar.classList.toggle('top');
+        flag = !flag;
+    }
+
+}, true);
+inputNombre.addEventListener('input', debounce(createMail));
+inputAsunto.addEventListener('input', debounce(createMail));
+textarea.addEventListener('input', debounce(createMail));
 
