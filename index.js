0cf8ada4b16edbc58a179ebd8af101d7dfb1dd34
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-13 15:19:18 -0300

    peron

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const btnEnviar = document.querySelector('[data-btn]');
-let flag = true;
+const btnEnviar = document.querySelector('[data-btn]'),
+      navbar = document.querySelector('.navbar');
 
+let flag = true;
 window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
 
     if(window.scrollY == 0 && !flag || window.scrollY > 0 && flag) {
         navbar.classList.toggle('top');
@@ -20,7 +20,7 @@ btnEnviar.addEventListener('click', event => {
             asunto : '',
             message: ''
           },
-          invalids = form.querySelectorAll('input:required,textarea:required');
+          invalids = form.querySelectorAll('input:required:invalid,textarea:required:invalid');
 
     if(invalids.length > 0) {
         const error = document.querySelector('[data-error]');
@@ -28,13 +28,22 @@ btnEnviar.addEventListener('click', event => {
         return;
     }
           
-    const data = form.querySelectorAll('input,textarea');
+    const data = form.querySelectorAll('input[type="text"],input[type="radio"]:checked,textarea');
 
     Array.from(data).forEach(item => {
         info[item.name] = item.value;
     });
 
-    const mail = `mailto:resolutioninfo.dev.com?Subject=${encodeURI(info.asunto)}&body=${info.nombre}${encodeURI(info.message)}`;
+    let mail;
+
+    if(info.mail == "gmail") {
+        mail = `https://mail.google.com/mail/u/0/?fs=1&to=resolveinfo.dev@gmail.com&su=${encodeURI(info.asunto)}&body=${info.nombre}${encodeURI(info.message)}&tf=cm`;
+    }
+
+    if(info.mail == "hotmail") {
+        mail = `mailto:resolveinfo.dev@gmail.com?Subject=${encodeURI(info.asunto)}&body=${info.nombre}${encodeURI(info.message)}`;
+    }
 
     console.log(mail)
 });
+
