30c866acd18725687f67a5d2da4fb90bea18950d
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-15 07:47:11 -0300

    changes responsive & add library smooth scroll

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+//-----------------------------------------------------------------------------
+// LIBRERIA PARA SMOOTH SCROLL
+
+SmoothScroll({
+    // Scrolling Core
+    animationTime    : 800, // [ms]
+    stepSize         : 100, // [px]
+
+    // Acceleration
+    accelerationDelta : 50,  // 50
+    accelerationMax   : 3,   // 3
+
+    // Keyboard Settings
+    keyboardSupport   : true,  // option
+    arrowScroll       : 50,    // [px]
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm   : true,
+    pulseScale       : 4,
+    pulseNormalize   : 1,
+
+    // Other
+    touchpadSupport   : false, // ignore touchpad by default
+    fixedBackground   : true, 
+    excluded          : ''    
+})
+
+//-----------------------------------------------------------------------------
+
 const btnEnviar   = document.querySelector('[data-btn]'),
       navbar      = document.querySelector('.navbar'),
       form        = document.querySelector('[data-form]'),
@@ -66,5 +96,3 @@ inputAsunto.addEventListener('input', debounce(createMail));
 textarea.addEventListener('input', debounce(createMail));
 
 //-----------------------------------------------------------------------------
-
-
