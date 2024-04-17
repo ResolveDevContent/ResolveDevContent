4d09ebb4f45ff354c32287f374d2ead04a2716c9
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-17 09:33:32 -0300

    animation text

--- index.js
+++ index.js
@@ -135,3 +135,49 @@ Array.from(mails).forEach(elm => {
 });
 
 //-----------------------------------------------------------------------------
+
+let words = ['Soluciones Tecnologicas.', 'Lionel Messi.', 'Edinson Cavani.'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 20,
+    speed = 120;
+
+let wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    document.querySelector(".word").innerText = part;
+  },speed);
+};
+
+  wordflick();
